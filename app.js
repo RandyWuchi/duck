30fa2784fc3fdd5626f3817b5858e10ca104967d
Randy Wuchi <nnadoziennawuchi@yahoo.com>
2021-12-20 17:35:12 +0200

    added the global error handler, request time and body parser middleware and not found route to app.js

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const app = express();
 
-// 1) Global Middlewares
+// 1) GLOBAL MIDDLEWARES
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Body Parser
+app.use(express.json());
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 2) ROUTES
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
