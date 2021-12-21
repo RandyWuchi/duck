c0406b6ffec8bcac7c7c61e5d9231d97786ee872
Randy Wuchi <nnadoziennawuchi@yahoo.com>
2021-12-21 16:15:31 +0200

    created a handler factory function for reading, writing, updating and deleting documents

---
+++ controllers/v1/handlerFactory.js
@@ -0,0 +1,107 @@
+const catchAsync = require('./../../utils/catchAsync');
+const AppError = require('./../../utils/appError');
+const APIFeatures = require('./../../utils/apiFeatures');
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // Allow nested GET request
+    let filter = {};
+    if (req.params.postId) filter = { post: req.param.postId };
+
+    // EXECUTE QUERY
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const docs = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      results: docs.length,
+      data: {
+        data: docs,
+      },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+
+    const doc = await query;
+
+    if (!doc) {
+      return next(
+        new AppError(
+          'No document with the given ID was found on this server',
+          404
+        )
+      );
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(
+        new AppError(
+          'No document with the given ID was found on this server',
+          404
+        )
+      );
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(
+        new AppError(
+          'No document with the given ID was found on this server',
+          404
+        )
+      );
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
