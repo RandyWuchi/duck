f6a5333bf7ef6e5b80bb5d4f9ba3b1f59d7205bf
Randy Wuchi <nnadoziennawuchi@yahoo.com>
2021-12-21 16:45:30 +0200

    defined the user routes

--- controllers/v1/userController.js
+++ controllers/v1/userController.js
@@ -6,3 +6,11 @@ exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User);
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
+
+exports.createUser = catchAsync(async (req, res, next) => {
+  res.status(500).json({
+    status: 'error',
+    message:
+      'This route is not defined and will never be defined! Please use /signup',
+  });
+});
--- routes/v1/userRoute.js
+++ routes/v1/userRoute.js
@@ -1,9 +1,23 @@
 const express = require('express');
 
-const authController = require('../../controllers/v1/authController');
+const authController = require('./../../controllers/v1/authController');
+const userController = require('./../../controllers/v1/userController');
 
 const router = express.Router();
 
+// AUTH ROUTES
 router.post('/signup', authController.signup);
+
+// USER ROUTES
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
