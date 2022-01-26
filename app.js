c05879028538e8a331119a5143e73f7abfefd158
Randy Wuchi <nnadoziennawuchi@yahoo.com>
2022-01-26 19:52:07 +0200

    installed pug for email templates

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/v1/errorController');
@@ -9,14 +14,33 @@ const postRouter = require('./routes/v1/postRoute');
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
+// Set security headers
+app.use(helmet());
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// Body Parser
+// Limit the rate of request from a particular IP to our API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body Parser, reading data from body into req.body
 app.use(express.json());
+
+// Data sanitization agains NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS attacks
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp());
 
 // Test middleware
 app.use((req, res, next) => {
--- utils/email.js
+++ utils/email.js
@@ -1,5 +1,37 @@
 const nodemailer = require('nodemailer');
 
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = 'Ruck Social <ruck@rucksocial.com>';
+  }
+
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return 1;
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // Send actual email
+  send(template, subject) {
+    // Render HTML template
+    // Define mail options
+    // Create transport and send email
+  }
+};
+
 const sendEmail = async (options) => {
   // Create a transport
   const transporter = nodemailer.createTransport({
@@ -22,5 +54,3 @@ const sendEmail = async (options) => {
   // Send the email
   await transporter.sendMail(mailOptions);
 };
-
-module.exports = sendEmail;
